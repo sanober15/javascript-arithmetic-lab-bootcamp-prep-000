b79fde42254998ee8782ff8d5d4c076ae97648ac
sanober15 <sanober@sanober.me>
2017-12-03 19:39:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,79 @@
+function add(a, b){
+  return a + b
+}
+function subtract(a,b){
+  return a - b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function inc(n){
+  n++
+  return n
+}
+function dec(n){
+  n--
+  return n
+}
+
+
+function makeInt(n){
+  n = parseInt('n',10)
+  if (n.toString() === 'n') {
+    n = '0'
+    }
+  else if ((isNaN(parseInt('n',10)))=== true) {
+      n = 'true'
+      }
+
+
+    /*
+  if (n=== NaN) {
+    n == isNaN('n')
+//    return console.log(n)
+  }
+  else if (n.toString() === n) {
+    return console.log();
+
+  }*/
+  else {
+    n = 0
+
+  }
+return n
+}
+/*
+describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})
+
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})
+*/
+function preserveDecimal(n){
+  n = parseFloat(n)
+  if (n === NaN) {
+    n = isNaN('n')
+  }
+  return n
+}
